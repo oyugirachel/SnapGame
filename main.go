b73bf487a9bde0a636fd7a43ce4b29b2f61bf559
oyugirachel <oyugirachel20@gmail.com>
2021-02-11 16:15:09 +0300

    implementing the score part

--- main.go
+++ main.go
@@ -3,24 +3,45 @@ package main
 import (
 	"fmt"
 
-	"github.com/oyugirachel/deck"
 	"github.com/common-nighthawk/go-figure"
+	"github.com/oyugirachel/deck"
 
 	"math/rand"
 	"time"
-
-	
 )
 
 // Holds the last two cards that will be displayed to the user
-var lastTwoCards [2]deck.Card
+
+var presentCards [2]deck.Card
+var lastCard = 1
 var score = 0
 var cardsDrawn = 0
+
+func checkLastTwoCards(snap bool) {
+	if snap {
+		if presentCards[0] == presentCards[1] {
+			// increment the score for the user has snapped
+			score++
+		} else {
+			// If the user snaps and the cards are not the same
+			score--
+		}
+	} else {
+		// Check if the last two cards are the same
+		if presentCards[0] == presentCards[1] {
+			// We are sure the user hasnt snapped so we deduct the score
+			score--
+		}
+
+	}
+	fmt.Println("\nYour score is:", score)
+
+}
 
 func main() {
 	cards := deck.New(deck.Deck(1), deck.Shuffle)
 	// Game instructions
-	art :=figure.NewColorFigure("SNAP GAME", "", "Red", true)
+	art := figure.NewColorFigure("SNAP GAME", "", "Red", true)
 	art.Blink(3000, 500, -1)
 
 	art.Print()
@@ -43,8 +64,8 @@ BE ON THE LOOKOUT !
 
 `
 	fmt.Println(message)
-	for k:=6; k>0; k--{
-		fmt.Printf("%d ..",k)
+	for k := 6; k > 0; k-- {
+		fmt.Printf("%d ..", k)
 		time.Sleep(time.Second)
 	}
 	fmt.Println("Gooooo!")
@@ -53,121 +74,46 @@ BE ON THE LOOKOUT !
 
 	rand.Seed(time.Now().UTC().UnixNano())
 	// calling in a goroutine to prevent blocking
-	go timedShuffle(cards)
+	// go timedShuffle(cards)
+	presentCards := []deck.Card{cards[0], cards[1]}
+	lastCard := 1
 
-	// fmt.Println(cards)
-	// fmt.Println(len(cards))
-	// fmt.Println(drawRandomCard(cards))
-	var input string
-
-	for {
-		if cardsDrawn == 52 {
-			break
-		}
-		
-		fmt.Scanf("%s\n", &input)
-		// fmt.Println(input)
-		if input != "" {
-			fmt.Println("SNAP")
-			checkLastTwoCards(true)
-
-		}
-
-		input = ""
-
-	}
-	fmt.Println("Players final score is :", score)
-
-}
-
-func checkLastTwoCards(snap bool) {
-	if snap {
-		if lastTwoCards[0] == lastTwoCards[1] {
-			// increment the score for the user has snapped
-			score++
-		} else {
-			// If the user snaps and the cards are not the same
-			score--
-		}
-	} else {
-		// Check if the last two cards are the same
-		if lastTwoCards[0] == lastTwoCards[1] {
-			// We are sure the user hasnt snapped so we deduct the score
-			score++
-		}
-
-	}
-	fmt.Println("\nYour score is:", score)
-
-}
-
-// DrawRandomCard function
-func drawRandomCard(cards []deck.Card) deck.Card {
-
-	// Generates a random card position between 0 and the length of the cards
-	var cardPosition = rand.Intn(len(cards))
-	// increment cards drawn
-	cardsDrawn++
-	
-	// fmt.Println(cardPosition)
-	// Returning the random chosen card
-
-	return cards[cardPosition]
-
-}
-
-// randInt function to randomize time between max and min
-func randInt(min int, max int) int {
-	return min + rand.Intn(max-min)
-}
-
-// timedShuffle function
-func timedShuffle(cards []deck.Card) {
-
-	// t := randInt(1, 5)
-	// x := time.Duration(t)
-
-	// // creating our timer and randomizing it
-
-	// timer := time.NewTimer(x * time.Second)
-
-	timer := time.NewTicker(time.Second * 2)
+	ticker := time.NewTicker(2 * time.Second)
+	inputChannel := make(chan string)
+	done := make(chan bool)
 
 	for {
+		go func() {
+			var input string
+			fmt.Scanf("%s", &input)
+			inputChannel <- input
+
+		}()
+
 		select {
-		// Waiting for the channel to emit a value
-		case <-timer.C:
-			// recursively call our shuffle
+		case <-ticker.C:
+			fmt.Printf("=============================[%2d/%2d]~ \n", lastCard, len(cards))
+			fmt.Println(presentCards[0])
+			fmt.Println(presentCards[1])
+			fmt.Println("============================")
 
-			// go timedShuffle(cards)
-
-			card := drawRandomCard(cards)
-			// shift position to position one
-			lastTwoCards[0] = lastTwoCards[1]
-			// taken the random card to be the most recent one
-			lastTwoCards[1] = card
-			checkLastTwoCards(false)
-
-			for index, j := range lastTwoCards {
-				
-				if index == 0 { //If the value is first one
-					fmt.Println("=====================================")
-					fmt.Printf("[ '%v', ", j)
-					
-				} else if len(lastTwoCards) == index+1 { // If the value is the last one
-					fmt.Printf("'%v' ] \n", j)
-					fmt.Println("======================================")
-				} else {
-					fmt.Printf(" '%v', ", j) // for all ( middle ) values
-				}
-				
+			lastCard++
+			if lastCard >= len(cards) {
+				done <- true
+				return
 			}
+			presentCards[0] = presentCards[1]
+			presentCards[1] = cards[lastCard]
+		case input := <-inputChannel:
+			if input == "" {
+				fmt.Println("SNAP")
+				checkLastTwoCards(true)
 
+			}
+		case <-done:
+			fmt.Println("Game over! your scored a total of ", score)
+			return
 		}
 	}
-}
-
-// scanInput function scans the input
-func scanInput() {
 
 }
